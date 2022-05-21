fa3ce459842ac420e08b980a89bda7bf0b739181
xav3428 <xavier.bouchard@hotmail.ca>
2022-05-21 05:18:49 -0400

    In Game UI Layout Done
    - Made the basic layout for the in-game layout

---
+++ Assets/Scripts/UI/UiTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiTimer : MonoBehaviour
+{
+    public TMP_Text TimerText;
+    public bool playing;
+    private float Timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(Timer / 60F);
+        int seconds = Mathf.FloorToInt(Timer % 60F);
+        int milliseconds = Mathf.FloorToInt((Timer * 100F) % 100F);
+        TimerText.text = minutes.ToString("00") + ":" + seconds.ToString("00") + ":" + milliseconds.ToString("00");
+
+    }
+}
---
+++ Assets/Scripts/WaveSystem.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSystem : MonoBehaviour
+{
+    // Variables
+    public static WaveSystem waveSystem;
+    int round = 0;
+    public int numberToSpawn = 4;
+    public int numberToAddEachRound = 4;
+    bool waveQueued = false;
+    public GameObject zombie;
+    private System.Random random = new System.Random();
+    private List<GameObject> listZombies = new List<GameObject>();
+    [SerializeField] private List<Vector3> listSpawnpoint = new List<Vector3>();
+    Vector3 spawnPoint;
+
+
+    // Accessible properties
+    public List<GameObject> ListZombies { get { return this.listZombies; } }
+    public int Round { get { return this.round; } }
+
+    /// <summary>
+    /// Functions
+    /// </summary>
+    private void Awake()
+    {
+        waveSystem = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (waveQueued == false && listZombies.Count == 0)
+        {
+            round += 1;
+            Invoke("startNextWave", 5f);
+            Debug.Log("round queued");
+            waveQueued = true;
+        }
+    }
+
+    void startNextWave()
+    {
+        spawnMonsters();
+        Debug.Log("Round" + round + "spawned");
+        waveQueued = false;
+    }
+
+    void spawnMonsters()
+    {
+        for (int i = 0; i < numberToSpawn; i++)
+        {
+            // The Invoke here makes it that every enemy spawns at an interval of 0.5f
+            Invoke("spawnEnemy", 0.5f*i);
+        }
+        numberToSpawn += numberToAddEachRound;
+    }
+
+    int generateRandomIndex()
+    {
+        // We roll a dice for a number between 1 to 3 to randomly select the next type of enemy to spawn
+        random = new System.Random();
+        return random.Next(listSpawnpoint.Count);
+    }
+
+    Vector3 selectRandomSpawnPoint()
+    {
+        return listSpawnpoint[generateRandomIndex()];
+    }
+
+    void spawnEnemy()
+    {
+        instantiateEnemy(zombie, selectRandomSpawnPoint());
+    }
+
+    void addEnemyToList(GameObject enemy)
+    {
+        listZombies.Add(enemy);
+    }
+
+    public void RemoveEnemyFromList(GameObject enemy)
+    {
+        listZombies.Remove(enemy);
+    }
+    void instantiateEnemy(GameObject monster, Vector3 appearanceTransform)
+    {
+        // We add the enemy to the monster list to easily have access to all monster later in the game
+        GameObject enemy = Instantiate(monster, appearanceTransform, Quaternion.identity);
+        addEnemyToList(enemy);
+        enemy.GetComponent<Enemy>().SetEnemyHP();
+    }
+}

2a0ed368a13f514be7b6aa45a956d8a81b3d2db4
xav3428 <xavier.bouchard@hotmail.ca>
2022-05-21 03:12:28 -0400

    Made Character Animation
    - Added character model
    
    - Made a animation blend tree for all direction
    
    - Added an AR in the player's hand for shooting later
    
    - Added a spotlight in front of the AR to light up the dark on the final map (will be dark)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,21 +8,33 @@ public class PlayerMovement : MonoBehaviour {
 
     float m_horizontal;
     float m_vertical;
-        
+    float forwardDirection;
+    float rightDirection;
+
     public float PlayerSpeed = 0.3f;
     [SerializeField]
     private float gravity = 9.81f;
 
+    private Animator animatorCharacter;
+
 	void Start () {
         m_charCont = GetComponent<CharacterController>();
+
+        animatorCharacter = GetComponent<Animator>();
 	}
 
 	void Update () {
         m_horizontal = Input.GetAxis("Horizontal");
         m_vertical = Input.GetAxis("Vertical");
+        forwardDirection = Vector3.Dot(transform.forward, new Vector3(m_horizontal, 0f, m_vertical));
+        rightDirection = Vector3.Dot(transform.right, new Vector3(m_horizontal, 0f, m_vertical));
+        animatorCharacter.SetFloat("Horizontal", rightDirection);
+        animatorCharacter.SetFloat("Vertical", forwardDirection);
 
         Vector3 m_playerMovement = new Vector3(m_horizontal, 0f, m_vertical) * PlayerSpeed * Time.deltaTime;
 
         m_charCont.Move(m_playerMovement);
-	}
+
+        
+    }
 }
